84f44e054fe18b0627496a4cbdeb7f728e6f8f4a
Vikum Deshan <vikumdeshan2k01@gmail.com>
2026-01-25 14:04:39 +0530

    implement Supabase media upload utility and add environment variables for API keys and URLs.

--- frontend/src/lib/mediaUpload.js
+++ frontend/src/lib/mediaUpload.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://fluldufbxekszalxvtkq.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsdWxkdWZieGVrc3phbHh2dGtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2NzY1MzIsImV4cCI6MjA4MDI1MjUzMn0.Bo1GWmi6soz0smUkKwNu0Pu6M38HGRgtu-wQojctQiQ"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
@@ -10,7 +10,7 @@ export default function uploadFile(file) {
     return new Promise((resolve, reject) => {
         const timeStamp = Date.now();
         const fileName = timeStamp + "_" + file.name;
-        supabase.storage.from("images").upload(fileName, file,{
+        supabase.storage.from("images").upload(fileName, file, {
             cacheControl: '3600',
             upsert: false
         }).then(() => {
@@ -21,4 +21,5 @@ export default function uploadFile(file) {
         });
 
 
-});    }
+    });
+}
